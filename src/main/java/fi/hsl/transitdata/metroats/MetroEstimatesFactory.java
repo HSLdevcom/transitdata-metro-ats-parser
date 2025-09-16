c0d38ad5563e8b0a7e8b1d857d2358b94ed47c7b
Alex <chihaiaalex@gmail.com>
2025-09-16 08:50:48 +0300

    61366-redis-cluster

--- src/main/java/fi/hsl/transitdata/metroats/MetroEstimatesFactory.java
+++ src/main/java/fi/hsl/transitdata/metroats/MetroEstimatesFactory.java
@@ -3,6 +3,7 @@ package fi.hsl.transitdata.metroats;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.hsl.common.mqtt.proto.Mqtt;
 import fi.hsl.common.pulsar.PulsarApplicationContext;
+import fi.hsl.common.redis.RedisStore;
 import fi.hsl.common.transitdata.TransitdataProperties;
 import fi.hsl.common.transitdata.TransitdataSchema;
 import fi.hsl.common.transitdata.proto.MetroAtsProtos;
@@ -13,10 +14,18 @@ import fi.hsl.transitdata.metroats.models.MetroTrainType;
 import org.apache.pulsar.client.api.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
 
-import java.time.*;
-import java.util.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class MetroEstimatesFactory {
@@ -31,13 +40,13 @@ public class MetroEstimatesFactory {
             ZoneId.of("Europe/Helsinki")
     );
 
-    private final Jedis jedis;
+    private final RedisStore redisStore;
     private final boolean addedTripsEnabled;
 
     private final EarlyDepartureLogger earlyDepartureLogger = new EarlyDepartureLogger(Duration.ofMinutes(5));
 
     public MetroEstimatesFactory(final PulsarApplicationContext context, boolean addedTripsEnabled) {
-        this.jedis = context.getJedis();
+        this.redisStore = context.getRedisStore();
         this.addedTripsEnabled = addedTripsEnabled;
         log.info("addedTripsEnabled set to: {}", this.addedTripsEnabled);
     }
@@ -181,7 +190,7 @@ public class MetroEstimatesFactory {
             metroEstimateBuilder.setOperatingDay(operatingDay);
             metroEstimateBuilder.setStartTime(startTime);
 
-            metroEstimateBuilder.setDvjId("metro-"+operatingDay+"-"+startTime+"-"+metroEstimate.routeName+"-"+metroEstimate.trainType.toString());
+            metroEstimateBuilder.setDvjId("metro-" + operatingDay + "-" + startTime + "-" + metroEstimate.routeName + "-" + metroEstimate.trainType.toString());
 
             metroEstimateBuilder.setScheduled(false);
         } else {
@@ -233,7 +242,7 @@ public class MetroEstimatesFactory {
         return maybeMetroTrainTypeAts;
     }
 
-    private Optional<MetroAtsProtos.MetroStopEstimate> toMetroStopEstimate (MetroStopEstimate metroStopEstimate, Integer direction, String beginTime, String startStopShortName, String routeName) {
+    private Optional<MetroAtsProtos.MetroStopEstimate> toMetroStopEstimate(MetroStopEstimate metroStopEstimate, Integer direction, String beginTime, String startStopShortName, String routeName) {
         final Optional<ZonedDateTime> metroStartTime = MetroUtils.parseMetroAtsDatetime(beginTime);
         if (metroStartTime.isEmpty()) {
             return Optional.empty();
@@ -328,30 +337,17 @@ public class MetroEstimatesFactory {
 
 
     private Optional<Map<String, String>> getMetroJourneyData(final String metroKey) {
-        synchronized (jedis) {
-            try {
-                Map<String, String> redisMap;
-                if (jedis.exists(metroKey)) {
-                    String keyType = jedis.type(metroKey);
-                    redisMap = jedis.hgetAll(metroKey);
-                    if (redisMap.isEmpty()) {
-                        log.debug("Couldn't find metroJourneyData from redis. Metro key: {}. Key type: {}", metroKey, keyType);
-                        return Optional.empty();
-                    } else {
-                        log.debug("Found metroJourneyData from redis. Metro key: {}. Key type: {}", metroKey, keyType);
-                    }
-                } else {
-                    log.error("Couldn't find key from jedis. Metro key: {}. Db size: {}", metroKey, jedis.dbSize());
-                    return Optional.empty();
-                }
-                if (redisMap != null && !redisMap.isEmpty()) {
-                    log.debug("Returning redisMap, size={}", redisMap.size());
-                }
-                return Optional.ofNullable(redisMap);
-            } catch (Exception e) {
-                log.error("Couldn't read metroJourneyData from redis. Metro key: {}", metroKey, e);
-                return Optional.empty();
+        try {
+            var journeyData = redisStore.getValues(metroKey);
+            if (journeyData.isEmpty()) {
+                log.debug("Couldn't find any data for metro key: {}", metroKey);
             }
+
+            return journeyData;
+
+        } catch (Exception e) {
+            log.error("Couldn't read metroJourneyData from redis. Metro key: {}", metroKey, e);
+            return Optional.empty();
         }
     }
 
